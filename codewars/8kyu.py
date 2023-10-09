c1e3651cb8b2ae3c41b299b98661a322c1610799
GuShls1 <54480226+AnisseHounaoui@users.noreply.github.com>
2023-10-08 20:10:07 -0400

    Update 8kyu.py

--- codewars/8kyu.py
+++ codewars/8kyu.py
@@ -1,2 +1,18 @@
-def kyu(test):
-  pass
+#Complete the solution so that it reverses the string passed into it.
+
+def solution(string):
+    return string[::-1] 
+  
+def solution(string):
+    l = []
+    for c in string:
+        l.insert(0,c) #add element c in position 0
+    return ''.join([c for c in l]) #join different element of the list by '' to form a string
+
+def solution(string):
+    reverse =''
+    for x in string:
+        reverse = x + reverse
+    return reverse
+
+
