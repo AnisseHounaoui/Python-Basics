4155aff6e816d973c8ca5c34b053385151d61ec0
GuShls1 <54480226+AnisseHounaoui@users.noreply.github.com>
2023-10-09 18:04:46 -0400

    Update 8kyu.py

--- codewars/8kyu.py
+++ codewars/8kyu.py
@@ -80,6 +80,12 @@ def paperwork(n, m): #best
     return max(n, 0) * max(m, 0)
 
 
+#Complete the square sum function so that it squares each number passed into it and then sums the results together.
+#For example, for [1, 2, 2] it should return 9
+
+def square_sum(numbers):
+    return sum(num**2 for num in numbers)
+
 
 
 
