5a90bfbe1283153ff78897ff182e7319fd5b3314
GuShls1 <54480226+AnisseHounaoui@users.noreply.github.com>
2023-10-08 20:22:25 -0400

    Update 8kyu.py

--- codewars/8kyu.py
+++ codewars/8kyu.py
@@ -15,4 +15,9 @@ def solution(string):
         reverse = x + reverse
     return reverse
 
+#Create a function which answers the question "Are you playing banjo?".
+#If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+def are_you_playing_banjo(name):
+    return name + " plays banjo" if name[0] == "r" or name[0] == "R" else name + " does not play banjo"
 
