3e6c6f9e76a9e18402bbe4018278281385795a59
GuShls1 <54480226+AnisseHounaoui@users.noreply.github.com>
2023-10-08 20:36:23 -0400

    Update 7kyu.py

--- codewars/7kyu.py
+++ codewars/7kyu.py
@@ -1,1 +1,15 @@
+#accum("abcd") -> "A-Bb-Ccc-Dddd"
+#accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+#accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
+def accum(s):
+    mumb = ""
+    s = s.upper()
+    i = 0
+    for e in s:
+        if i==0:
+            mumb += e + "-" #first character doesnt have duplicate
+        else:
+            mumb += e + (e.lower())*i + "-" 
+        i += 1
+    return mumb[:-1] #remove last character "-"
