390d9ea80aff367c18b33b197acab2dc90434632
GuShls1 <54480226+AnisseHounaoui@users.noreply.github.com>
2023-10-09 16:49:42 -0400

    Update 7kyu.py

--- codewars/7kyu.py
+++ codewars/7kyu.py
@@ -17,5 +17,16 @@ def accum(s):
 def accum(s):
     return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s)) #enumerate() is like a dictionarry we can get inexes and values
 
+#Your task is to return the number of people who are still on the bus after the last bus stop (after the last array).
 
+def number(bus_stops):
+    inb = 0
+    outb = 0
+    for i in range(len(bus_stops)):
+        inb += bus_stops[i][0]
+        outb += bus_stops[i][1]
+    return inb - outb
+
+def number(bus_stops): #best
+    return sum([stop[0] - stop[1] for stop in bus_stops])
 
